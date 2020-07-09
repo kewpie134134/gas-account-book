a395052e9f8c29f7f84c69162cb00367898a2aa0
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-07-09 23:28:20 +0900

    add CRUD in client side

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ Vue.use(Vuex);
  * Vuex の状態
  */
 const state = {
+  /** 家計簿データ */
+  abData: {},
   /** 設定 */
   settings: {
     appName: "GAS 家計簿",
@@ -25,6 +27,35 @@ const state = {
  * Actions から State を更新するときに呼ばれる
  */
 const mutations = {
+  /** 指定年月の家計簿データをセットする */
+  setAbData(state, { yearMonth, list }) {
+    state.abData[yearMonth] = list;
+  },
+  /** データを追加する */
+  addAbData(state, { item }) {
+    const yearMonth = item.date.slice(0, 7);
+    const list = state.abData[yearMonth];
+    if (list) {
+      list.push(item);
+    }
+  },
+  /** 指定年月のデータを更新する */
+  updateAbData(state, { yearMonth, item }) {
+    const list = state.abData[yearMonth];
+    if (list) {
+      const index = list.findIndex((v) => v.id === item.id);
+      list.splice(index, 1, item);
+    }
+  },
+  /** 指定年月とID のデータを削除する */
+  deleteAbData(state, { yearMonth, id }) {
+    const list = state.abData[yearMonth];
+    if (list) {
+      const index = list.findIndex((v) => v.id === id);
+      list.splice(index, 1);
+    }
+  },
+
   /** 設定を保存する */
   saveSettings(state, { settings }) {
     state.settings = { ...settings };
@@ -47,6 +78,57 @@ const mutations = {
  * 画面から呼ばれ、Mutation をコミットする
  */
 const actions = {
+  /** 指定年月の家計簿データを取得する */
+  fetchAbData({ commit }, { yearMonth }) {
+    // サンプルデータを初期値として入れる
+    const list = [
+      {
+        id: "a34109ed",
+        date: `${yearMonth}-01`,
+        title: "支出サンプル",
+        category: "買い物",
+        tags: "タグ1",
+        income: null,
+        outgo: 2000,
+        memo: "メモ",
+      },
+      {
+        id: "7c8fa764",
+        date: `${yearMonth}-02`,
+        title: "収入サンプル",
+        category: "給料",
+        tags: "タグ1,タグ2",
+        income: 2000,
+        outgo: null,
+        memo: "メモ",
+      },
+    ];
+    commit("setAbData", { yearMonth, list });
+  },
+  /** データを追加する */
+  addAbData({ commit }, { item }) {
+    commit("addAbData", { item });
+  },
+  /** データを更新する */
+  updateAbData({ commit }, { beforeYM, item }) {
+    const yearMonth = item.date.slice(0, 7);
+    // 更新前後で年月の変更が無ければそのまま値を更新
+    if (yearMonth === beforeYM) {
+      commit("updateAbData", { yearMonth, item });
+      return;
+    }
+    // 更新があれば、更新前年月のデータから削除して、新しくデータを追加する
+    const id = item.id;
+    commit("deleteAbData", { yearMonth: beforeYM, id });
+    commit("addAbData", { item });
+  },
+  /** データを削除する */
+  deleteAbData({ commit }, { item }) {
+    const yearMonth = item.date.slice(0, 7);
+    const id = item.id;
+    commit("deleteAbData", { yearMonth, id });
+  },
+
   /** 設定を保存する */
   saveSettings({ commit }, { settings }) {
     commit("saveSettings", { settings });
