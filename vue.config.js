5fbadacfc6192c655a4f186e718a659963714c8e
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-07-12 18:17:48 +0900

    GitHubPages で画面が表示されるように修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  // GitHubPages で画面が真っ白にならないように修正
+  publicPath: "./",
   // GitHubPages に公開するディレクトリ指定
+  // ★ Firebase に公開する際は、コメントアウトして "dist"ディレクトリを作成
   outputDir: "docs/",
 };
