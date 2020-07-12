d25daddfe27b4f220bbb60b69255badc51ab4c5f
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-07-12 18:32:23 +0900

    yarn build で基本は "dist" にアウトプットするよう修正

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   // GitHubPages で画面が真っ白にならないように修正
   publicPath: "./",
-  // GitHubPages に公開するディレクトリ指定
-  // ★ Firebase に公開する際は、コメントアウトして "dist"ディレクトリを作成
-  outputDir: "docs/",
+  // GitHubPages に公開する際は、"docs/"ディレクトリを指定する
+  // Firebase に公開する際は、"dist/"ディレクトリを指定する
+  outputDir: "dist/",
 };
